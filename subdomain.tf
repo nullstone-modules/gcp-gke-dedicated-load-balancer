8c55b16ed24f5016d6592d99d45667be6c1a5518
Brad Sickles <brad.sickles@gmail.com>
2023-04-19 09:21:01 -0400

    Add subdomain connection and cluster app connection

---
+++ subdomain.tf
@@ -0,0 +1,9 @@
+data "ns_connection" "subdomain" {
+  name     = "subdomain"
+  contract = "subdomain/gcp/cloud-dns"
+}
+
+locals {
+  subdomain_fqdn    = data.ns_connection.subdomain.outputs.fqdn
+  subdomain_zone_id = data.ns_connection.subdomain.outputs.zone_id
+}
