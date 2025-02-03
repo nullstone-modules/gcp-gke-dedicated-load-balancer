1a1da78e26c0ea3ba086762f233b8cc59f6ca4cb
Brad Sickles <brad.sickles@gmail.com>
2025-02-03 18:55:07 -0500

    Use health check parameters for readiness probe

--- outputs.tf
+++ outputs.tf
@@ -17,9 +17,10 @@ output "readiness_probes" {
   value = [
     {
       initial_delay_seconds = 0
-      period_seconds        = 1
-      timeout_seconds       = 1
-      success_threshold     = 1
+      period_seconds        = var.health_check_interval
+      timeout_seconds       = var.health_check_timeout
+      success_threshold     = var.health_check_healthy_threshold
+      failure_threshold     = var.health_check_unhealthy_threshold
       http_get = jsonencode({
         path = var.health_check_path
         port = local.container_port
